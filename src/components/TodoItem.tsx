9b065fa9b2e30ebd0386ce6edd10fe281d6668ad
confidentmeerkat <confidentmeerkat@gmail.com>
2023-04-26 22:19:36 -0500

    Change status change button into checkbox

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,8 +1,8 @@
-import TrashIcon from "@heroicons/react/24/solid/TrashIcon";
+import { ChangeEvent, useEffect, useState } from "react";
 import { DataStore } from "@aws-amplify/datastore";
-import { Todo } from "../models";
 import classNames from "classnames";
-import { useEffect, useState } from "react";
+import TrashIcon from "@heroicons/react/24/solid/TrashIcon";
+import { Todo } from "../models";
 
 interface IProps {
   item: Todo;
@@ -15,7 +15,7 @@ const TodoItem: React.FC<IProps> = ({ item }) => {
     item.category.then((category) => {
       setCategoryName(category?.name || "");
     });
-  }, []);
+  }, [item.category]);
 
   const handleDelete = async () => {
     try {
@@ -25,11 +25,11 @@ const TodoItem: React.FC<IProps> = ({ item }) => {
     }
   };
 
-  const handleToggleStatus = async () => {
+  const handleChangeStatus = async (e: ChangeEvent<HTMLInputElement>) => {
     try {
       await DataStore.save(
         Todo.copyOf(item, (updated) => {
-          updated.status = item.status === "FINISHED" ? "UNFINISHED" : "FINISHED";
+          updated.status = e.target.checked ? "FINISHED" : "UNFINISHED";
         })
       );
     } catch (e) {
@@ -45,15 +45,16 @@ const TodoItem: React.FC<IProps> = ({ item }) => {
       <div className="flex flex-1 flex-col">
         <div className="flex items-center">
           <p
-            className={classNames("text-sm font-semibold leading-6 text-gray-900", {
+            className={classNames("text-sm font-semibold leading-6", {
               "line-through text-gray-500": item.status === "FINISHED",
+              "text-gray-900": item.status === "UNFINISHED",
             })}
           >
             {item.name}
           </p>
 
           {categoryName && (
-            <span className="rounded-full bg-blue-500 text-white px-1 py-px text-[10px] ml-1 h-fit">
+            <span className="rounded-full bg-blue-500 text-white px-2 py-px text-[10px] ml-1 h-fit">
               {categoryName}
             </span>
           )}
@@ -61,13 +62,13 @@ const TodoItem: React.FC<IProps> = ({ item }) => {
         <p className="text-sm text-gray-600">{item.description}</p>
       </div>
 
-      <dl className="flex gap-x-8 sm:w-auto items-center">
-        <button
-          className="bg-indigo-600 p-1.5 hover:bg-indigo-500 rounded-md text-xs text-white w-28"
-          onClick={handleToggleStatus}
-        >
-          Mark as {item.status === "FINISHED" ? "Undone" : "Done"}
-        </button>
+      <dl className="flex gap-x-2 sm:w-auto items-center">
+        <input
+          checked={item.status === "FINISHED"}
+          onChange={handleChangeStatus}
+          type="checkbox"
+          className="w-7 h-7 rounded-md border-2 border-yellow-600 focus:ring-transparent checked:bg-green-600 checked:hover:bg-green-500 checked:focus:bg-green-600"
+        />
 
         <button className="bg-red-600 p-1.5 hover:bg-red-500 rounded-md" onClick={handleDelete}>
           <TrashIcon className="w-4 h-4 text-white " />
